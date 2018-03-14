dd4dd67d5f68fa1252246c53b5deb8541ae19cc7
Leeza Luncheon <leezaluncheon@gmail.com>
2018-03-14 01:29:45 +0000

    automatically backed up by learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,10 @@
 def welcome
-  # puts out a welcome message here!
+puts "Welcome to..."
+puts  "    8888888888  888    88888\n   88     88   88 88   88  88\n    8888  88  88   88  88888\n       88 88 888888888 88   88\n88888888  88 88     88 88    888888\n\n 88  88  88   888    88888    888888\n88  88  88  88 88   88  88  88\n88 8888 88 88   88  88888    8888\n 888  888 888888888 88   88     88\n  88  88  88     88 88    8888888"
 end
 
 def get_character_from_user
-  puts "please enter a character"
-  # use gets to capture the user's input. This method should return that input, downcased.
+  puts "\nPlease enter a character (or 'q' to quit):"
+  gets.chomp
+
 end
