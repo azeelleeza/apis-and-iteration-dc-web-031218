2ca1667f4b042cfcdaa65e799c3289f61deadeec
Leeza Luncheon <leezaluncheon@gmail.com>
2018-03-14 01:30:04 +0000

    automatically backed up by learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,10 +1,37 @@
-def welcome
-puts "Welcome to..."
-puts  "    8888888888  888    88888\n   88     88   88 88   88  88\n    8888  88  88   88  88888\n       88 88 888888888 88   88\n88888888  88 88     88 88    888888\n\n 88  88  88   888    88888    888888\n88  88  88  88 88   88  88  88\n88 8888 88 88   88  88888    8888\n 888  888 888888888 88   88     88\n  88  88  88     88 88    8888888"
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+def next_page(count)
+  people_api = 'http://www.swapi.co/api/people/'
+  current_page = JSON.parse(RestClient.get("#{people_api}?page=#{count}"))
+  if current_page["next"]!= nil
+    current_page["results"]
+  else
+    puts "No such character."
+    do_it
+  end
 end
 
-def get_character_from_user
-  puts "\nPlease enter a character (or 'q' to quit):"
-  gets.chomp
+def get_character_movies_from_api(character)
+  found = nil
+  count = 1
 
+  until found
+    found = next_page(count).find{|hashobj|hashobj["name"]==character}
+    count +=1
+  end
+
+  found["films"].map{ |film_api| JSON.parse(RestClient.get(film_api))} if found !=nil
+end
+
+
+def parse_character_movies(films_hash)
+  puts "\n#{films_hash.map{|film| film["title"]}.join("\n").upcase}\n"
+end
+
+def show_character_movies(character)
+  films_hash = get_character_movies_from_api(character)
+  parse_character_movies(films_hash)
 end
